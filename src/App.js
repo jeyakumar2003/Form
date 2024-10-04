baeb41678a5e8acd29459aeeaf3309b4bf0bb57c
jeyakumar2003 <jeya44727@gmail.com>
2024-10-04 03:39:43 -0700

    Added re-usable card & button, validition & resetting logics, list of users and errorModal.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-
+import AddUser from './Components/User/AddUser';
+import UserList from './Components/User/UserList';
 
 
 function App() {
+  const [UsersList, setUserList] =useState([]);
+
+  const addUserHandler = (uName, uAge) =>{
+    setUserList((prevUserList) =>{
+      return[...prevUserList,{name :uName, age: uAge, id: Math.random.toString()}];
+    } );
+  }
   return (
     <div>
-      <h1>wdoiiluqwbfniphbj</h1>
-  
+      <AddUser onAddUser ={addUserHandler}/>
+      <UserList users={UsersList}/>
     </div>
   );
 }
